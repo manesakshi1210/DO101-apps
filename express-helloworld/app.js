b7122a51ff9135168b73dfee4704a5abb3e0e411
Sakshi Navnath Mane <sakshinavnathmane12@gmail.com>
2025-02-04 09:21:28 +0000

    add mars/endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,8 +4,8 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
-
+app.get(m)
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
-})
+});
 
